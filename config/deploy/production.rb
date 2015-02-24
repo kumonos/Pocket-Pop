5dc8720673673abba7516802f1c09497e2fd2f85
muratayusuke <info@muratayusuke.com>
2015-02-24 09:48:35 +0900

    add roles

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,6 @@
 set :deploy_to, '/apps/pocketporter'
 set :unicorn_config_path, File.join(fetch(:deploy_to), 'current', 'config', 'unicorn.rb')
+
+role :app, 'localhost'
+role :web, 'localhost'
+role :db,  'localhost'
