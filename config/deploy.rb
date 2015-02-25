f46d758458957403c6d4da2e8b33ea8a9a1329ca
muratayusuke <info@muratayusuke.com>
2015-02-25 20:48:54 +0900

    fix rbenv path

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,9 +6,6 @@ set :branch, ENV['BRANCH'] || 'master'
 # set up rbenv
 set :rbenv_type, :user
 set :rbenv_ruby, '2.1.1'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} " \
-  "RBENV_VERSION=#{fetch(:rbenv_ruby)} " \
-  "#{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all # default value
 
