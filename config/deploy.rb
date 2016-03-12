bb67345befe0ea07791f20105c9aa3b11c0bef1d
muratayusuke <info@muratayusuke.com>
2016-03-12 13:17:19 +0900

    Update ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :branch, ENV['BRANCH'] || 'master'
 
 # set up rbenv
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.1'
+set :rbenv_ruby, '2.2.2'
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all # default value
 
